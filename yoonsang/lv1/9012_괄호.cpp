7fd73847a85c0a2b319f5a05e86e9a59aec2b7c2
yoonsang Lee <highp0912@pm.me>
2024-03-04 22:27:32 +0900

    add: 실4 괄호

---
+++ yoonsang/lv1/9012_괄호.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<char> split(string s)
+{
+    vector<char> ret;
+    for (char c : s)
+    {
+        ret.push_back(c);
+    }
+    return ret;
+}
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+
+    vector<string> answer;
+    answer.reserve(t);
+
+    for (int i = 0; i < t; ++i)
+    {
+        string str;
+        cin >> str;
+
+        stack<char> st;
+        auto splitted = split(str);
+        for (auto const &s : splitted)
+            st.push(s);
+
+        int vps = 0;
+        while (!st.empty())
+        {
+            const auto token = st.top();
+            st.pop();
+
+            if (token == ')')
+                ++vps;
+            else
+            {
+                --vps;
+            }
+        }
+
+        answer.push_back(vps == 0 ? "YES" : "NO");
+    }
+
+    for (int i = 0; i < answer.size(); ++i)
+    {
+        cout << answer[i];
+        if (i < answer.size())
+        {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
