3f9e0e79f7d9beb0a3c3021b177ded2a9f9c4270
yoonsang Lee <highp0912@pm.me>
2024-03-10 17:13:45 +0900

    add: 균형잡힌 세상 풀이

---
+++ yoonsang/bj/4949_균형잡힌_세상.cpp
@@ -0,0 +1,63 @@
+// https://www.acmicpc.net/problem/4949
+// 4949 균형잡힌 세상
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool contains(const string &from, const string &str) {
+  return from.find(str) != string::npos;
+}
+
+void prob() {
+  ios::sync_with_stdio(false);
+
+  string line;
+  while (getline(cin, line)) {
+    if (line == ".")
+      break;
+
+    bool failed = false;
+    stack<char> st;
+    for (const char c : line) {
+      if (c == '(' || c == '[') {
+        st.push(c);
+        continue;
+      }
+
+      if (c == ']') {
+        if (!st.empty() && st.top() == '[') {
+          st.pop();
+          continue;
+        }
+
+        failed = true;
+        break;
+      }
+
+      if (c == ')') {
+        if (!st.empty() && st.top() == '(') {
+          st.pop();
+          continue;
+        }
+
+        failed = true;
+        break;
+      }
+    }
+
+    if (!failed && st.empty()) {
+      cout << "yes\n";
+    } else {
+      cout << "no\n";
+    }
+  }
+}
+
+int main() {
+  ::prob();
+
+  return 0;
+}
---
+++ yoonsang/bj/9012_괄호.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<char> split(string s) {
+  vector<char> ret;
+  for (char c : s) {
+    ret.push_back(c);
+  }
+  return ret;
+}
+
+int main() {
+  int t = 0;
+  cin >> t;
+
+  vector<string> answer;
+  answer.reserve(t);
+
+  for (int i = 0; i < t; ++i) {
+    string str;
+    cin >> str;
+
+    stack<char> st;
+    auto splitted = split(str);
+    for (auto const &s : splitted)
+      st.push(s);
+
+    int vps = 0;
+    while (!st.empty()) {
+      const auto token = st.top();
+      st.pop();
+
+      if (token == ')')
+        ++vps;
+      else {
+        --vps;
+        if (vps < 0)
+          break;
+      }
+    }
+
+    answer.push_back(vps == 0 ? "YES" : "NO");
+  }
+
+  for (int i = 0; i < answer.size(); ++i) {
+    cout << answer[i];
+    if (i < answer.size()) {
+      cout << endl;
+    }
+  }
+
+  return 0;
+}
