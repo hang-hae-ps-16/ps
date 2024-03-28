914b29559769b4ef56f6131b7c1080644a8deb52
yoonsang Lee <highp0912@pm.me>
2024-03-28 14:33:55 +0900

    clang 셋업

--- yoonsang/bj/9012_괄호.cpp
+++ yoonsang/bj/9012_괄호.cpp
@@ -5,22 +5,26 @@
 
 using namespace std;
 
-vector<char> split(string s) {
+vector<char> split(string s)
+{
   vector<char> ret;
-  for (char c : s) {
+  for (char c : s)
+  {
     ret.push_back(c);
   }
   return ret;
 }
 
-void prob2() {
+void prob2()
+{
   int t = 0;
   cin >> t;
 
   vector<string> answer;
   answer.reserve(t);
 
-  for (int i = 0; i < t; ++i) {
+  for (int i = 0; i < t; ++i)
+  {
     string str;
     cin >> str;
 
@@ -30,13 +34,15 @@ void prob2() {
       st.push(s);
 
     int vps = 0;
-    while (!st.empty()) {
+    while (!st.empty())
+    {
       const auto token = st.top();
       st.pop();
 
       if (token == ')')
         ++vps;
-      else {
+      else
+      {
         --vps;
         if (vps < 0)
           break;
@@ -46,15 +52,17 @@ void prob2() {
     answer.push_back(vps == 0 ? "YES" : "NO");
   }
 
-  for (int i = 0; i < answer.size(); ++i) {
+  for (int i = 0; i < answer.size(); ++i)
+  {
     cout << answer[i];
-    if (i < answer.size()) {
+    if (i < answer.size())
+    {
       cout << endl;
     }
   }
 }
 
-//int main() {
-//  ::prob();
-//  return 0;
-//}
+// int main() {
+//   ::prob();
+//   return 0;
+// }
